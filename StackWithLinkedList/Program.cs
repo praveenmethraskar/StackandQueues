b06f5e9e5643603fb946f4b63875d956b53b56c5
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-19 17:56:59 +0530

    dequeue method in queue concept

--- StackWithLinkedList/Program.cs
+++ StackWithLinkedList/Program.cs
@@ -9,22 +9,32 @@ namespace StackWithLinkedList
 
         public static void Main(string[] args)
         {
-            Stack_Ex stackobj = new Stack_Ex();
-            stackobj.Push(20);
-            stackobj.Push(30);
-            stackobj.Push(40);
-            stackobj.Display();
-            stackobj.peek();
-            stackobj.pop();
-            stackobj.Display();
+            //Stack_Ex stackobj = new Stack_Ex();
+            //stackobj.Push(20);
+            //stackobj.Push(30);
+            //stackobj.Push(40);
+            //stackobj.Display();
+            //stackobj.peek();
+            //stackobj.pop();
+            //stackobj.Display();
 
-            stackobj.peek();
-            stackobj.pop();
-            stackobj.Display();
+            //stackobj.peek();
+            //stackobj.pop();
+            //stackobj.Display();
 
-            stackobj.peek();
-            stackobj.pop();
-            stackobj.Display();
+            //stackobj.peek();
+            //stackobj.pop();
+            //stackobj.Display();
+
+
+            QueueEx queueEx = new QueueEx();
+            queueEx.Enqueue(56);
+            queueEx.Enqueue(30);
+            queueEx.Enqueue(70); 
+            queueEx.Display();
+
+            queueEx.Dequeue();
+            queueEx.Display();
 
         }
     }
