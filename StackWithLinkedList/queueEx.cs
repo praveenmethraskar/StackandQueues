4e79133af5827e8fc69cd2952b8f243d5a473e69
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-19 17:54:26 +0530

    Queue elements insertion

--- StackWithLinkedList/queueEx.cs
+++ StackWithLinkedList/queueEx.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace StackWithLinkedList
 {
-    internal class queueEx
+    public class QueueEx
     {
+        Node rear;
+        Node front;
+
+        public void Enqueue(int data)
+        {
+            Node newnode = new Node(data);
+            if (front == null)
+            {
+                rear = newnode;
+                front = rear;
+            }
+            else
+            {
+                rear.Next = newnode;
+                rear = rear.Next;
+            }
+        }
+
+        public void Display()
+        {
+            if (front == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            Node temp = front;
+            while (temp != null)
+            {
+                Console.WriteLine(" "+temp.data+" ");
+                temp = temp.Next;
+            }
+        }
+
+ 
     }
 }
